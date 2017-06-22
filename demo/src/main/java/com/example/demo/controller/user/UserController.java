8c5dd77fdd428120bd16cc4ca8222910b6637da6
caocc <only3cj@163.com>
2017-06-22 11:16:10 +0800

    调试热部署

--- demo/src/main/java/com/example/demo/controller/user/UserController.java
+++ demo/src/main/java/com/example/demo/controller/user/UserController.java
@@ -2,11 +2,15 @@ package com.example.demo.controller.user;
 
 import java.util.List;
 
+import org.codehaus.groovy.util.Finalizable;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.User;
@@ -47,8 +51,13 @@ public class UserController {
     }
 
     @RequestMapping("/s/all")
-    @SerializedField(excludes = {"id","password"})
+    @SerializedField(excludes = {"id","loginPassword"})
     public List<User> findAllUser(){
         return userRepository.findAll();
     }
+    
+    @RequestMapping(value="/page",method=RequestMethod.POST)
+    public Page<User> page(final Pageable pageable) {
+    	return userRepository.findAll(pageable);
+	}
 }
--- demo/src/main/java/com/example/demo/repository/UserRepository.java
+++ demo/src/main/java/com/example/demo/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.example.demo.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.model.User;
@@ -9,4 +11,6 @@ import com.example.demo.model.User;
 public interface UserRepository extends JpaRepository<User, Long> {
 	
 	List<User> findAllByLoginNameContainingOrderByIdDesc(String loginName);
+	
+	Page<User> findAll(Pageable pageable);
 }
